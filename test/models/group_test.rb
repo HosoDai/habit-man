e91e764432ffe72e43c1d793620ced383354bc2c
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-08-15 23:11:53 +0900

    finish test models/groups

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @group = Group.new(name: "Example Group", owner_id: 1)
+  end
+
+  test "should be valid" do
+    assert @group.valid?
+  end
+
+  test "name should be present" do
+    @group.name = " "
+    assert @group.invalid?
+  end
+
+  test "name should not be too long" do
+    @group.name = "a" * 51
+    assert @group.invalid?
+  end
 end
