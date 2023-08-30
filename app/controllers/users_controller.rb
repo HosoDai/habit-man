8e319527e82e35dca4128ccfcec057ec644b2585
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-08-30 22:16:25 +0900

    fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -45,6 +46,12 @@ class UsersController < ApplicationController
     # beforeフィルター
 
     # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 
     # 正しいユーザーかどうか確認
     def correct_user
