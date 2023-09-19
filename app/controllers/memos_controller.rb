ce099dd884c8dfe2701b0150c08ffb6c96bfbd14
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-09-19 23:08:48 +0900

    fix memos create function

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,11 +5,12 @@ class MemosController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @memo = @group.memos.new(memo_params)
+    @date = params[:memo][:memo_date]
     @memo.user_id = current_user.id
     @memo.group_id = @group.id
     # あとでmemo_dateを保存する記述を追加
     @memo.memo_date = params[:memo][:memo_date].to_date
-    @pagy, @memos = pagy(@group.memos.where(memo_date: params[:memo][:memo_date].to_date).order(updated_at: :desc))
+    @pagy, @memos = pagy(@group.memos.where(memo_date: params[:memo][:memo_date].to_date))
     if @memo.save
       flash[:success] = "You succeeded in creating new memo!"
       # redirect_to group_memo_path(@group, @memo)
