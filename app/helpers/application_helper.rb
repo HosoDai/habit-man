3d42cb76da722fe2e2f32afb67889aa199798971
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-10-09 21:43:31 +0900

    fix app title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返す
   def full_title(page_title = '')
-    base_title = "Member Management App"
+    base_title = "Habit man"
     if page_title.empty?
       base_title
     else
