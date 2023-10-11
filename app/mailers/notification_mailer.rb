fb457e37f69c67ab34d8eeae9a6e9065584563a5
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-10-11 17:23:14 +0900

    fix notification mail subject

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
   def notification_for_member(notification, group)
     @notification = notification
     @group = group
-    mail bcc: @group.users.pluck(:email), subject: @notification.title
+    mail bcc: @group.users.pluck(:email), subject: "#{@notification.title} from #{@notification.user.name}"
   end
 end
