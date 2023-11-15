7df73e32ac10dc0f0eb5796036c37fec3cf8a8b5
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-11-15 09:07:03 +0900

    fix migration file

--- db/migrate/20231110112416_change_column_to_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToNull < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :memos, :accessed_at, true
-  end
-end
--- db/migrate/20231115000544_add_accessed_at_to_memos.rb
+++ db/migrate/20231115000544_add_accessed_at_to_memos.rb
@@ -1,5 +1,5 @@
 class AddAccessedAtToMemos < ActiveRecord::Migration[7.0]
   def change
-    add_column :memos, :accessed_at, :datetime, null: false
+    add_column :memos, :accessed_at, :datetime
   end
 end
