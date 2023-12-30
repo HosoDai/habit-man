197e02c3672bf1b00dc33a7b6710a02a2d3ab9bf
HosoDai <78740209+HosoDai@users.noreply.github.com>
2023-12-30 15:00:35 +0900

    fix comment-notice

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,12 +5,16 @@ class NotificationMailer < ApplicationMailer
     mail bcc: @group.users.pluck(:email), subject: "#{@notification.title} from #{@notification.user.name}"
   end
 
-  def comment_notification(notification, comment, group)
-    @notification = notification
+  def comment_notification(memo, comment, group)
+    @memo = memo
     @comment = comment
     @group = group
-    @notification.memo_comments.each do |memo_comment|
-      mail(to: memo_comment.user.email, subject: "#{@comment.comment} from #{@comment.user.name}")
+    array_email = []
+    @memo.memo_comments.each do |memo_comment|
+      unless
+        array_email.append(memo_comment.user.email)
+      end
     end
+    mail bcc: array_email, subject: "#{@comment.comment} from #{@comment.user.name}"
   end
 end
